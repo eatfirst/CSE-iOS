bd4f661f1d6441bceb65861e812b6b009b8b3b85
Karthikeya Udupa K M <karthikeyaudupa@gmail.com>
2016-09-16 13:27:16 +0200

    added class to handle paypal authorisation

---
+++ ADYEncryption/ADYPayPalPayment.h
@@ -0,0 +1,13 @@
+//
+//  ADYPayPalPayment.h
+//  AdyenClientsideEncryptionDemo
+//
+//  Created by Karthikeya Udupa on 16/09/16.
+//  Copyright © 2016 Adyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADYPayPalPayment : NSObject
+
+@end
---
+++ ADYEncryption/ADYPayPalPayment.m
@@ -0,0 +1,13 @@
+//
+//  ADYPayPalPayment.m
+//  AdyenClientsideEncryptionDemo
+//
+//  Created by Karthikeya Udupa on 16/09/16.
+//  Copyright © 2016 Adyen. All rights reserved.
+//
+
+#import "ADYPayPalPayment.h"
+
+@implementation ADYPayPalPayment
+
+@end
